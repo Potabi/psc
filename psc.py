5e355ea64d2f69d0faad48a65d71dc7ae44dee4f
Kai <Kai>
2021-11-24 21:33:27 -0700

    Add whoami function (configures user.name and user.email)

--- psc.py
+++ psc.py
@@ -5,7 +5,7 @@ import os
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
 
-base_commands=["clone","pull","commit","push"]
+base_commands=["clone","pull","commit","push","whoami"]
 
 # psc commit <dir> "message" push
 if len(sys.argv) < 2:
@@ -40,6 +40,7 @@ def scmrun(command,position):
         pass
     elif(command == "push"):
         if int(int(position)+1) == len(sys.argv):
+            pass
             os.system("git push")
         else:
             if int(len(sys.argv) - int(int(position)+1)) == int(2):
@@ -48,5 +49,28 @@ def scmrun(command,position):
                 os.system("git push " + remote + " " + branch)
             else:
                 exit
+    elif(command == "whoami"):
+        def whoami_error():
+            print("ERROR: whoami was not run properly")
+            print("The Whoami function needs to be ran independently")
+            print("Usage: psc whoami <global/local> <name> <email>")
+            print("\tglobal: Set this to be for all repositories")
+            print("\tlocal: Set this only for this repository")
+            exit
+        if int(position) is not int(1):
+            whoami_error()
+        elif int(len(sys.argv)) < 4 or int(len(sys.argv)) > 5:
+            whoami_error()
+        else:
+            if str(sys.argv[int(int(position)+1)]) == str("global"):
+                os.system("git config --global user.name " + str(sys.argv[int(int(position)+2)]))
+                os.system("git config --global user.email " + str(sys.argv[int(int(position)+2)]))
+                print("Success")
+            elif str(sys.argv[int(int(position)+1)]) == str("local"):
+                os.system("git config user.name " + str(sys.argv[int(int(position)+2)]))
+                os.system("git config user.email " + str(sys.argv[int(int(position)+2)]))
+                print("Success")
+            else:
+                whoami_error()
 
 checkcommand(str(sys.argv[1]),int(1))
